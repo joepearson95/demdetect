dafb501d67ec375e27deec24d812e0c6374ace54
joepearson95 <joe.c.pearson95@gmail.com>
2020-10-09 15:05:21 +0100

    Beginning of data preprocessing has started.

--- demdetect.py
+++ demdetect.py
@@ -7,8 +7,9 @@ from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog
 import cv2
 import numpy as np
+import pandas as pd
 # Image obtained
-im = cv2.imread("input2.jpeg")
+im = cv2.imread("input3.jpeg")
 #cv2.imshow('ImageWindow',im)
 
 # Create config
@@ -21,9 +22,19 @@ cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Keypoints/keypoint_rcnn_R
 # Predictor created
 predictor = DefaultPredictor(cfg)
 outputs = predictor(im)
+
+# Map keypoints to image
 v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
 out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-print(outputs['instances'].pred_keypoints)
+
+# Get keypoints and create dataframe before passing to csv
+output = outputs['instances'].pred_keypoints.to("cpu")
+create_keypoint_dataset = pd.DataFrame(output.tolist())
+#print(create_keypoint_dataset)
+create_keypoint_dataset.to_csv('keypoints.csv')
 #cv2.imshow('', out.get_image()[:, :, ::-1])
 #cv2.waitKey(0)
-#print(outputs)
+
+# TODO: 
+# Go through all the 'valid'  photos, extract skeleton data. Save to a file
+# Build the next model, train it etc.
