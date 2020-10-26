8238ee79c78a0ec09872ab34e5a819fe50185ce3
joepearson95 <joe.c.pearson95@gmail.com>
2020-10-26 18:42:59 +0000

    Updates

--- action_rec_model.py
+++ action_rec_model.py
@@ -29,16 +29,14 @@ class LSTM(nn.Module):
         return out
 
 class DemDataset(Dataset):
-    def __init__(self, csv_file):
+    def __init__(self, csv_file, transform=None):
         self.df = pd.read_csv(csv_file)
         del self.df['file_name']
+        self.transform = transform
     def __len__(self):
         return len(self.df)
 
     def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
-        
         self.x = torch.Tensor(self.df.iloc[idx,:-4].astype('float64'))
         self.y = torch.Tensor(self.df.iloc[idx, -4:].astype('float64'))
 
@@ -46,61 +44,71 @@ class DemDataset(Dataset):
         
 # Get the dataset ready
 csv_file = '#'
-demdataset = DemDataset(csv_file)
 
+mean, std = 207.1851, 161.2207
+demdataset = DemDataset(csv_file)#, transform=transforms.Compose([transforms.Normalize(mean, std)]))
+
+from sklearn.model_selection import train_test_split    
+trainset, testset = train_test_split(demdataset, test_size=0.2, shuffle=True)
 # Get the data split
-train_size = int(0.8 * len(demdataset))
-test_size = len(demdataset) - train_size
-trainset, testset = random_split(demdataset, [train_size, test_size])
+# train_size = int(0.8 * len(demdataset))
+# test_size = len(demdataset) - train_size
+# trainset, testset = random_split(demdataset, [train_size, test_size])
 
 # Hyper Param
 input_size = 51
 sequence_length = 1
 batch_size = 4
-hidden_size = 200
-num_layers = 2
+hidden_size = 50
+num_layers = 1
 num_classes = 4
-no_epochs = 4
-lr_rate = 0.0001
+no_epochs = 50
+lr_rate = 0.01
 
 # # Create the dataloader
-training_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
+training_loader = DataLoader(trainset,  batch_size=batch_size, shuffle=True)
 testing_loader = DataLoader(testset, batch_size=batch_size, shuffle=False)
 demdetect = LSTM(input_size, hidden_size, num_layers, num_classes).to(device)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(demdetect.parameters(), lr=lr_rate)
 demdetect.train()
+
+epoch_loss = []
+count_loss = []
+total = 0
+correct = 0
 for epochs in range(no_epochs):
     ep_loss = 0.0
     run_loss = 0.0
     for i, (points, labels) in enumerate(training_loader):
         points = points.reshape(-1, sequence_length, input_size).to(device)
+        point_norm = transforms.Normalize(207.1851, 161.2207)
+        points = point_norm(points)
         labels = labels.to(device)
-
         optimizer.zero_grad()
         
         outputs = demdetect(points)
         loss = criterion(outputs, torch.max(labels,1)[1])
         loss.backward()
         optimizer.step()
-        ep_loss += outputs.shape[0]*loss.item()
-        run_loss += loss.item()
-        if (i+1) % 100 == 0:
-            print('[Epoch %d of %d] Running Loss: %.3f' % (epochs+1, no_epochs, run_loss/100))
-            run_loss = 0.0
-    print('[Epoch %d of %d] Epoch Loss: %.3f' % (epochs+1, no_epochs, ep_loss/len(trainset)))
-print('Finished Training.')
 
-correct = 0
-total = 0
+        run_loss += loss.item()
+        ep_loss += loss.item()
+    epoch_loss.append(ep_loss/len(training_loader))
+    print('[Epoch %d of %d] Epoch Loss: %.3f' % (epochs+1, no_epochs, ep_loss/len(training_loader)))
+print('Finished Training.')
+# plt.plot(epoch_loss)
+# plt.show()
 demdetect.eval()
 with torch.no_grad():
     for i, (points, labels) in enumerate(testing_loader):
         points = points.reshape(-1, sequence_length, input_size).to(device)
+        point_norm = transforms.Normalize(207.1851, 161.2207)
+        points = point_norm(points)
         labels = labels.to(device)
+        total += labels.shape[0]
         outputs = demdetect(points)
-        _, predicted = outputs.max(1)
-        correct +=  (predicted == labels).sum()
-        total += predicted.size(0)
-    print(f'Got {correct}/{total} with accuracy {float(correct)/float(total)*100:.2f}')
+        correct += torch.sum(labels == outputs.argmax(dim=-1))
+acc = float(correct)/float(total)
+print('%.3f%%' % (100*acc))
