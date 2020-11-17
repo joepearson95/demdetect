1e2923848322b10dcdc2d3572223ccfbcdadec03
joepearson95 <joe.c.pearson95@gmail.com>
2020-11-17 18:17:59 +0000

    Completed the first papers equation.

--- normalise.py
+++ normalise.py
@@ -1,10 +1,10 @@
 import pandas as pd
+import numpy as np
 
 
 class Normalise:
     """
-    A class to normalise the skeleton data given,
-    depending on specific algorithm.
+    A class to normalise the skeleton data given based on specific papers equations.
     """
 
     def __init__(self, csv_file):
@@ -13,13 +13,17 @@ class Normalise:
         self.df = self.df.iloc[:, :-3]
         self.no_score = self.df[self.df.columns.drop(list(self.df.filter(regex='score')))]
 
-    def paper1(self):
+    # The Hindawi papers algorithm for normalising data
+    def hindawi(self):
+        # Create feature vectors for each point in the dataset
         feature_vector = []
-        for i in range(17):  # Use this when wanting to do the whole skeleton
+        posture_vector = []
+        for i in range(17):
             feature_vector.append([
                 self.no_score["point_" + str(i) + "_x"][0],
                 self.no_score["point_" + str(i) + "_y"][0],
             ])
+        # Extract the three joints below using the midpoint formula
         neck_joint = [
             (feature_vector[5][0] + feature_vector[5][1]) / 2,
             (feature_vector[6][0] + feature_vector[6][1]) / 2
@@ -32,16 +36,24 @@ class Normalise:
             (neck_joint[0] + neck_joint[1]) / 2,
             (hip_middle[0] + hip_middle[1]) / 2,
         ]
-        # The below are for inserting into original array
-        # feature_vector.insert(6, neck_joint)
-        # feature_vector.insert(12, hip_middle)
-        # feature_vector.insert(8, torso_middle)
+        # Create the denominator by creating a distance vector and
+        # calculating its norm
+        distance_vector2 = [
+            (neck_joint[0] + neck_joint[1]) / 2,
+            (torso_middle[0] + torso_middle[1]) / 2
+        ]
+        norm = np.linalg.norm(distance_vector2)
 
-# print(self.df[self.df.columns['point_'+str(i)+'_y']][0])
-# print(self.df[self.df.columns['point_'+str(i)+'_score']][0])
-# col_names.append("point_" + str(x) + "_x")
-#     col_names.append("point_" + str(x) + "_y")
-#     col_names.append("point_" + str(x) + "_score")
+        # Loop the feature vectors and normalise each point
+        # returning a posture feature vector
+        for x in feature_vector:
+            distance_vector1 = [
+                (x[0] + x[1]) / 2,
+                (torso_middle[0] + torso_middle[1] / 2)
+            ]
+            posture_vector.append(distance_vector1 / norm)
+
+        return posture_vector
 
 
 file = '#'
