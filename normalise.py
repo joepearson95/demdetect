a688fcce7095ed1acbd70e42f4fbef62da7d1b59
joepearson95 <joe.c.pearson95@gmail.com>
2020-11-18 13:13:52 +0000

    Added second paper algorithm.

--- normalise.py
+++ normalise.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class Normalise:
     """
-    A class to normalise the skeleton data given based on specific papers equations.
+    A class to normalise the skeleton data given.
     """
 
     def __init__(self, csv_file):
@@ -55,7 +55,53 @@ class Normalise:
 
         return posture_vector
 
+    # Loop a vector starting with specific index
+    def start_with(self, arr, start_index):
+        for i in range(start_index, len(arr)):
+            yield arr[i]
+        for i in range(start_index):
+            yield arr[i]
+
+    # The Hal paper for normalisation
+    def hal(self):
+        # Create a feature vector of points and find the hip joint
+        feature_vector = []
+        hip_sub = []
+        for i in range(17):
+            feature_vector.append([
+                self.no_score["point_" + str(i) + "_x"][0],
+                self.no_score["point_" + str(i) + "_y"][0],
+            ])
+        hip_middle = [
+            (feature_vector[11][0] + feature_vector[11][1]) / 2,
+            (feature_vector[12][0] + feature_vector[12][1]) / 2,
+        ]
+        # Based on bio-mechanics, the hip is subtracted from each point
+        for x in feature_vector:
+            hip_sub.append(np.subtract(x, hip_middle))
+
+        # Starting with a section of the hip joint and moving to the others,
+        # find the normalised euclidean distance. Adaptinge each subsequent
+        # vector for its specific usage
+        num = 1
+        euc_norm = []
+        vel_norm = []
+        acc_norm = []
+        kf = []
+        for val in self.start_with(hip_sub, 11):
+            euc_norm.append(np.linalg.norm(val))
+            vel_norm.append(np.linalg.norm(val + 1) - np.linalg.norm(val - 1))
+            acc_norm.append(np.linalg.norm(val + 2) + np.linalg.norm(val - 2) - 2 * np.linalg.norm(val))
+            num += 1
+
+        # Create Kinematic Features vector based on joint position,
+        # joint velocity and joint acceleration
+        kf.append(euc_norm)
+        kf.append(vel_norm)
+        kf.append(acc_norm)
+        return kf
+
 
 file = '#'
 normalise = Normalise(file)
-normalise.paper1()
+normalise.hal()
