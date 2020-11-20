5ffe6fb038d25c838c89ee202fb90b0915b7a2f6
joepearson95 <joe.c.pearson95@gmail.com>
2020-11-20 15:40:11 +0000

    Resolved iteration of each row issue.

--- normalise.py
+++ normalise.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 
-
 class Normalise:
     """
     A class to normalise the skeleton data given.
@@ -12,57 +11,53 @@ class Normalise:
         del self.df['file_name']
         self.classes = self.df[self.df.columns[-3:]]
         self.df = self.df.iloc[:, :-3]
-        self.no_score = self.df[self.df.columns.drop(list(self.df.filter(regex='score')))]
+        self.no_score = self.df[self.df.columns.drop(
+            list(self.df.filter(regex='score')))]
 
     # The Hindawi papers algorithm for normalising data
     def hindawi(self):
         # Create feature vectors for each point in the dataset
         feature_vector = []
         posture_vector = []
-        for i in range(17):
-            feature_vector.append([
-                self.no_score["point_" + str(i) + "_x"],
-                self.no_score["point_" + str(i) + "_y"],
-            ])
-        # Extract the three joints below using the midpoint formula
-        neck_joint = [
-            (feature_vector[5][0] + feature_vector[5][1]) / 2,
-            (feature_vector[6][0] + feature_vector[6][1]) / 2
-        ]
-        hip_middle = [
-            (feature_vector[11][0] + feature_vector[11][1]) / 2,
-            (feature_vector[12][0] + feature_vector[12][1]) / 2,
-        ]
-        torso_middle = [
-            (neck_joint[0] + neck_joint[1]) / 2,
-            (hip_middle[0] + hip_middle[1]) / 2,
-        ]
-        # Create the denominator by creating a distance vector and
-        # calculating its norm
-        distance_vector2 = [
-            (neck_joint[0] + neck_joint[1]) / 2,
-            (torso_middle[0] + torso_middle[1]) / 2
-        ]
-        norm = np.linalg.norm(distance_vector2)
-
-        # Loop the feature vectors and normalise each point
-        # returning a posture feature vector
-        for x in feature_vector:
-            distance_vector1 = [
-                (x[0] + x[1]) / 2,
-                (torso_middle[0] + torso_middle[1] / 2)
+        dataset_len = len(self.no_score)
+        point_len = 17
+        for i in range(dataset_len):
+            current_arr = []
+            for j in range(point_len):
+                current_arr.append((
+                    self.no_score["point_" + str(j) + "_x"].values[i],
+                    self.no_score["point_" + str(j) + "_y"].values[i]
+                ))
+            feature_vector.append(current_arr)
+        # Loop the dataset, calculating the three missing joints.
+        # Next, the two distance vectors for this iteration,
+        # before normalising the result and appending to []
+        for x in range(dataset_len):
+            neck_joint = [
+                (feature_vector[x][5][0] + feature_vector[x][6][0]) / 2,
+                (feature_vector[x][5][1] + feature_vector[x][6][1]) / 2
             ]
-            posture_vector.append(distance_vector1 / norm)
-
+            hip_middle = [
+                (feature_vector[x][11][0] + feature_vector[x][11][1]) / 2,
+                (feature_vector[x][12][0] + feature_vector[x][12][1]) / 2,
+            ]
+            torso_middle = [
+                (neck_joint[0] + neck_joint[1]) / 2,
+                (hip_middle[0] + hip_middle[1]) / 2,
+            ]
+            curr_pos = []
+            for y in range(point_len):
+                dv1 = np.linalg.norm(np.asarray((feature_vector[x][y][0], feature_vector[x][y][1])) - np.asarray((torso_middle[0], torso_middle[1])))
+                dv2 = np.linalg.norm(np.asarray((neck_joint[0], neck_joint[1])) - np.asarray((torso_middle[0], torso_middle[1])))
+                curr_pos.append(dv1 / dv2)
+            posture_vector.append(curr_pos)
         # Create the dataframe to pass to model
         col_names = []
         for x in range(17):
-            col_names.append("point_" + str(x) + "_x")
-            col_names.append("point_" + str(x) + "_y")
-        df = pd.DataFrame(sum(map(list, posture_vector), []))
-        hindawiDF = pd.DataFrame(df.T.values, columns=col_names)
+            col_names.append("point_" + str(x))
+        hindawiDF = pd.DataFrame(posture_vector, columns=col_names)
         hind_con = pd.concat([hindawiDF, self.classes], axis=1)
-        print(hind_con)
+        return hind_con
 
     # Loop a vector starting with specific index
     def start_with(self, arr, start_index):
@@ -97,7 +92,7 @@ class Normalise:
         vel_norm = []
         acc_norm = []
         kf = []
-        for val in self.start_with(hip_sub, 11):
+        for val in self.start_with(feature_vector, 11):
             euc_norm.append(np.linalg.norm(val))
             vel_norm.append(np.linalg.norm(val + 1) - np.linalg.norm(val - 1))
             acc_norm.append(np.linalg.norm(val + 2) + np.linalg.norm(val - 2) - 2 * np.linalg.norm(val))
@@ -114,12 +109,13 @@ class Normalise:
             col_names.append("point_" + str(x) + "_x")
             col_names.append("point_" + str(x) + "_y")
 
-        # df = pd.DataFrame(sum(map(list, posture_vector), []))
-        # hindawiDF = pd.DataFrame(df.T.values, columns=col_names)
-        # pd.concat([hindawi_norm[0], hindawi_norm[1]], axis=1)
-        print(kf)
+        # euc_df = pd.DataFrame(euc_norm)
+        df = pd.DataFrame(euc_norm)
+        hindawiDF = pd.DataFrame(df.T.values)
+        # hind_con = pd.concat([hindawiDF, self.classes], axis=1)
+        print(euc_norm)
 
 
 file = '#'
 normalise = Normalise(file)
-normalise.hal()
+normalise.hindawi()
