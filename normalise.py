bf44ba3e74663bda4a900c4d68754e227a8b6a1b
joepearson95 <joe.c.pearson95@gmail.com>
2020-11-21 16:29:38 +0000

    Slowly getting there with the second algorithm issue...

--- normalise.py
+++ normalise.py
@@ -59,7 +59,6 @@ class Normalise:
         hind_con = pd.concat([hindawiDF, self.classes], axis=1)
         return hind_con
 
-    # Loop a vector starting with specific index
     def start_with(self, arr, start_index):
         for i in range(start_index, len(arr)):
             yield arr[i]
@@ -71,51 +70,77 @@ class Normalise:
         # Create a feature vector of points and find the hip joint
         feature_vector = []
         hip_sub = []
-        for i in range(17):
-            feature_vector.append([
-                self.no_score["point_" + str(i) + "_x"],
-                self.no_score["point_" + str(i) + "_y"],
-            ])
-        hip_middle = [
-            (feature_vector[11][0] + feature_vector[11][1]) / 2,
-            (feature_vector[12][0] + feature_vector[12][1]) / 2,
-        ]
-        # Based on bio-mechanics, the hip is subtracted from each point
-        for x in feature_vector:
-            hip_sub.append(np.subtract(x, hip_middle))
+        dataset_len = 2#len(self.no_score)
+        point_len = 17
+        for i in range(dataset_len):
+            current_arr = []
+            for j in range(point_len):
+                current_arr.append((
+                    self.no_score["point_" + str(j) + "_x"].values[i],
+                    self.no_score["point_" + str(j) + "_y"].values[i]
+                ))
+            feature_vector.append(current_arr)
+        for x in range(dataset_len):
+            hip_middle = [
+                (feature_vector[x][11][0] + feature_vector[x][11][1]) / 2,
+                (feature_vector[x][12][0] + feature_vector[x][12][1]) / 2,
+            ]
+            # Based on bio-mechanics, the hip is subtracted from each point
+            for y in range(point_len):
+                hip_sub.append(np.subtract(feature_vector[x][y], hip_middle))
+            # Starting with a section of the hip joint and moving to the others,
+            # find the normalised euclidean distance. Adaptinge each subsequent
+            # vector for its specific usage
 
-        # Starting with a section of the hip joint and moving to the others,
-        # find the normalised euclidean distance. Adaptinge each subsequent
-        # vector for its specific usage
-        num = 1
-        euc_norm = []
-        vel_norm = []
-        acc_norm = []
-        kf = []
-        for val in self.start_with(feature_vector, 11):
-            euc_norm.append(np.linalg.norm(val))
-            vel_norm.append(np.linalg.norm(val + 1) - np.linalg.norm(val - 1))
-            acc_norm.append(np.linalg.norm(val + 2) + np.linalg.norm(val - 2) - 2 * np.linalg.norm(val))
-            num += 1
+            reordered = []
+            norm_pos = []
+            velocity = []
+            acceleration = []
+            # loop the array, on out of bounds, specfically ask for the array item
+            for j in self.start_with(hip_sub, 11):
+                reordered.append(j)
+            
+            # Due to out of bounds index, these are done before the iterator
+            velocity.append((reordered[1]/np.linalg.norm(reordered[1]) - (reordered[16]/np.linalg.norm(reordered[16]))))
+            acceleration.append((reordered[2]/np.linalg.norm(reordered[2])) + (reordered[15]/np.linalg.norm(reordered[15]))-2*(reordered[0]/np.linalg.norm(reordered[0])))
+            acceleration.append((reordered[3]/np.linalg.norm(reordered[3])) + (reordered[16]/np.linalg.norm(reordered[16]))-2*(reordered[1]/np.linalg.norm(reordered[1])))
+            for idx, val in enumerate(reordered):
+                norm = np.linalg.norm(reordered[idx])
+                norm_pos.append(reordered[idx]/norm)
+                if idx + 1 <= len(reordered) - 1 and idx - 1 >= 0:
+                    norm_add = np.linalg.norm(reordered[idx + 1])
+                    norm_subtract = np.linalg.norm(reordered[idx - 1])
+                    velocity.append((reordered[idx + 1]/norm_add) - (reordered[idx - 1]/norm_subtract))
+                if idx + 2 <= len(reordered) - 1 and idx - 2 >= 0:
+                    norm_add_acc = np.linalg.norm(reordered[idx + 2])
+                    norm_subtract_acc =  np.linalg.norm(reordered[idx - 2])
+                    acceleration.append((reordered[idx + 2]/norm_add_acc) + (reordered[idx - 2]/norm_subtract_acc)-2*(reordered[idx]/norm))
+            velocity.append((reordered[0]/np.linalg.norm(reordered[0])) - (reordered[15]/np.linalg.norm(reordered[15])))
+            acceleration.append((reordered[0]/np.linalg.norm(reordered[0])) + (reordered[13]/np.linalg.norm(reordered[13]))-2*(reordered[15]/np.linalg.norm(reordered[15])))
+            acceleration.append((reordered[1]/np.linalg.norm(reordered[1])) + (reordered[14]/np.linalg.norm(reordered[14]))-2*(reordered[16]/np.linalg.norm(reordered[16])))
 
-        # Create Kinematic Features vector based on joint position,
-        # joint velocity and joint acceleration
-        kf.append(euc_norm)
-        kf.append(vel_norm)
-        kf.append(acc_norm)
-
+            # Create Kinematic Features vector based on joint position,
+            # joint velocity and joint acceleration
         col_names = []
         for x in range(17):
-            col_names.append("point_" + str(x) + "_x")
-            col_names.append("point_" + str(x) + "_y")
+            col_names.append("point_" + str(x))
+            # col_names.append("point_" + str(x) + "_y")
 
-        # euc_df = pd.DataFrame(euc_norm)
-        df = pd.DataFrame(euc_norm)
-        hindawiDF = pd.DataFrame(df.T.values)
-        # hind_con = pd.concat([hindawiDF, self.classes], axis=1)
-        print(euc_norm)
+        kf = []
+        kf.append(norm_pos)
+        kf.append(velocity)
+        kf.append(acceleration)
+        
+        # df = np.array(kf).flatten()
+        print(norm_pos) #(pd.DataFrame(df).transpose())
+        # print(pd.DataFrame(kf, columns=col_names))
+
+        # # euc_df = pd.DataFrame(euc_norm)
+        # df = pd.DataFrame(euc_norm)
+        # hindawiDF = pd.DataFrame(df.T.values)
+        # # hind_con = pd.concat([hindawiDF, self.classes], axis=1)
+        # print(euc_norm)
 
 
 file = '#'
-normalise = Normalise(file)
-normalise.hindawi()
+normalise.hal()
