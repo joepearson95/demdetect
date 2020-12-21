0da5b1c56c5871968e7a0c5e700a350fe66bbc5e
joepearson95 <joe.c.pearson95@gmail.com>
2020-12-21 19:57:35 +0000

    Accuracy slowly improving

--- cnn.py
+++ cnn.py
@@ -15,15 +15,17 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        self.conv1 = torch.nn.Conv1d(in_channels=1, out_channels=4, kernel_size=17)
+        self.conv1 = torch.nn.Conv1d(1, 8, 17)
         self.activ1 = torch.nn.ReLU()
-        self.conv2 = torch.nn.Conv1d(in_channels=4,out_channels=3,kernel_size=1)
-        
+        self.conv2 = torch.nn.Conv1d(8,3,1)
+        self.activ2 = torch.nn.ReLU()
+
     def forward(self, x):
         x = self.conv1(x)
         x = self.activ1(x)
         x = self.conv2(x)
-
+        x = self.activ2(x)
+        
         log = torch.nn.functional.softmax(x, dim=1)
 
         return log
@@ -58,7 +60,7 @@ demdetect = CNN()
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(demdetect.parameters(), lr=0.0001)
 
-no_epochs = 5
+no_epochs = 20
 num_correct = 0
 num_samples = 0
 t_num_correct = 0
@@ -82,7 +84,7 @@ for epochs in range(no_epochs):
             optimizer.step()
 
             _, predictions = outputs.max(1)
-            num_correct += (predictions == torch.max(labels, 1)[1]).sum()
+            num_correct += (predictions == torch.max(labels)).sum()
             num_samples += predictions.size(0)
             running_loss += loss.item()
     train_acc = float(num_correct) / float(num_samples) * 100
@@ -97,7 +99,7 @@ for epochs in range(no_epochs):
         if labels.shape == (3,3):
             labels = np.reshape(torch.max(labels, 1)[1], (3,1))
             _, predictions = outputs.max(1)
-            t_num_correct += (predictions == torch.max(labels, 1)[1]).sum()
+            t_num_correct += (predictions == torch.max(labels)).sum()
             t_num_samples += predictions.size(0)
     test_acc = float(t_num_correct) / float(t_num_samples) * 100
     test_err.append((test_acc - 100) * -1)
