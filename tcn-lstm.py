33c3605cfd8629742d4de1045160535241c593a2
joepearson95 <joe.c.pearson95@gmail.com>
2021-01-13 15:58:31 +0000

    sorting out the combined architecture. However, I have a headache so its going slowly.

--- tcn-lstm.py
+++ tcn-lstm.py
@@ -2,23 +2,55 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
+from torch.nn import Softmax
+import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 from sklearn.model_selection import train_test_split
 import normalise
+import numpy as np
 
 # GPU if applicable
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
+def layer_conv(in_conv,out_conv, size_of_kernel):
+    conv = nn.Sequential(
+        torch.nn.Conv1d(in_conv, out_conv, kernel_size=size_of_kernel),
+        torch.nn.ReLU()
+    )
+    return conv
 
-class LSTM(nn.Module):
+class TCN(nn.Module):
+    def __init__(self):
+        super(TCN, self).__init__()
+        self.first_layer = torch.nn.Conv1d(90,3,1)
+
+        # Upsampling
+        self.up_samp = torch.nn.ConvTranspose1d(
+            in_channels=3,
+            out_channels=90,
+            kernel_size=1,
+        )
+        self.uplayer1 = torch.nn.Conv1d(90,3,1)
+
+    def forward(self, x):
+        # encoder
+        encode1 = F.relu(self.first_layer(x))
+        pooled = F.max_pool1d(encode1,1)
+        # decoder
+        x = self.up_samp(pooled)
+        x = F.relu(self.uplayer1(x))
+        return F.softmax(x, dim=1)
+
+class TCNLSTM(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, num_classes):
-        super(LSTM, self).__init__()
-        self.num_layers = num_layers
-        self.hidden_size = hidden_size
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, num_classes)
+	    super(TCNLSTM, self).__init__()
+	    self.TCN = TCN()
+	    self.num_layers = num_layers
+	    self.hidden_size = hidden_size
+	    self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+	    self.fc = nn.Linear(hidden_size, num_classes)
 
     def forward(self, x):
         # hidden state and cell state
@@ -43,9 +75,6 @@ class DemDataset(Dataset):
 
         return self.x, self.y
 
-
-# def hindawi_norm()
-
 # Get the dataset ready
 csv_file = '../demcare1_ingest_dataset.csv'
 normaliser = normalise.Normalise(csv_file)
@@ -56,17 +85,17 @@ trainset, testset = train_test_split(demdataset, test_size=0.2, shuffle=False)
 # Hyper Param
 input_size = 90
 sequence_length = 1
-batch_size = 5
+batch_size = 
 hidden_size = 3
 num_layers = 1
 num_classes = 3
-no_epochs = 10
+no_epochs = 20
 lr_rate = 0.001
 
 # Create the dataloader
 training_loader = DataLoader(trainset, batch_size=batch_size, shuffle=False)
 testing_loader = DataLoader(testset, batch_size=batch_size, shuffle=False)
-demdetect = LSTM(input_size, hidden_size, num_layers, num_classes).to(device)
+demdetect = TCNLSTM(input_size, hidden_size, num_layers, num_classes).to(device)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(demdetect.parameters(), lr=lr_rate)
@@ -112,8 +141,4 @@ for epochs in range(no_epochs):
     test_err.append((test_acc - 100) * -1)
     print(f'[Epoch {epochs + 1}/{no_epochs}] Epoch Loss: {ep_loss / len(training_loader):.3f} | Got {num_correct} of {num_samples} with accuracy {train_acc:.2f}%')
     print(f'[Epoch {epochs + 1}/{no_epochs}] | Test: Got {num_correct} of {t_num_samples} with accuracy {test_acc:.2f}%')
-# print('Finished Training.')
-
-# # print(len(train_acc_list), len(test_acc_list))
-# # plt.plot(train_err, 'r', test_err, 'g')
-# # plt.show()
+print('Finished Training.')
