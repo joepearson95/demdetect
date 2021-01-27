7d15cc8aa22366bd9d44b02dbe383c031aec6667
joepearson95 <joe.c.pearson95@gmail.com>
2021-01-27 13:38:52 +0000

    slowly sorting the 'frames' issue.

--- tcn.py
+++ tcn.py
@@ -41,11 +41,11 @@ class DemDataset(Dataset):
         self.transform = transform
 
     def __len__(self):
-        return len(self.data)
+        return len(self.data) - 1
 
     def __getitem__(self, idx):
-        self.x = torch.Tensor(self.data.iloc[idx, :-3])
-        self.y = torch.Tensor(self.data.iloc[idx, -3:])
+        self.x = torch.Tensor(self.data.iloc[idx:idx+1, :-3])
+        self.y = torch.Tensor(self.data.iloc[idx+1, -3:])
 
         return self.x, self.y
 
@@ -56,69 +56,73 @@ class DemDataset(Dataset):
 csv_file = '../demcare1_ingest_dataset.csv'
 normaliser = normalise.Normalise(csv_file)
 normaliserAlg = normaliser.hal()
+
 demdataset = DemDataset(normaliserAlg)
 trainset, testset = train_test_split(demdataset, test_size=0.2, shuffle=False)
-
 training_loader = DataLoader(trainset, batch_size=3, shuffle=False)
 testing_loader = DataLoader(testset, batch_size=3, shuffle=False)
-demdetect = TCN()
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(demdetect.parameters(), lr=0.001)
 
-no_epochs = 30
-num_correct = 0
-num_samples = 0
-t_num_correct = 0
-t_num_samples = 0
-train_err = []
-test_err = []
+for i, (points, labels) in enumerate(training_loader):
+    print(points.shape)
+# demdetect = TCN()
+# criterion = nn.CrossEntropyLoss()
+# optimizer = optim.Adam(demdetect.parameters(), lr=0.001)
 
-# Begin training
-for epochs in range(no_epochs):
-    running_loss = 0.0
-    demdetect.train()
-    for i, (points, labels) in enumerate(training_loader):
-        points = points.to(device)
-        points = points.unsqueeze_(2)
-        labels = labels.to(device)
-        outputs = demdetect(points)
+# no_epochs = 30
+# num_correct = 0
+# num_samples = 0
+# t_num_correct = 0
+# t_num_samples = 0
+# train_err = []
+# test_err = []
+
+# # Begin training
+# for epochs in range(no_epochs):
+#     running_loss = 0.0
+#     demdetect.train()
+#     for i, (points, labels) in enumerate(training_loader):
+#         points = points.to(device)
+#         print(points.shape)
+#         points = points.unsqueeze_(2)
+#         labels = labels.to(device)
+#         outputs = demdetect(points)
         
-        labels = torch.max(labels, 1)[1]
+#         labels = torch.max(labels, 1)[1]
 
-        if outputs.size() == 3:
-            outputs = outputs.reshape(3,1,3,1)
-        outputs = outputs.flatten(start_dim=1)
-        loss = criterion(outputs, labels)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+#         if outputs.size() == 3:
+#             outputs = outputs.reshape(3,1,3,1)
+#         outputs = outputs.flatten(start_dim=1)
+#         loss = criterion(outputs, labels)
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
 
-        _, predictions = outputs.max(1)
-        num_correct += (predictions ==  labels).sum()
-        num_samples += predictions.size(0)
-        running_loss += loss.item()
-    train_acc = float(num_correct) / float(num_samples) * 100
-    train_err.append((train_acc - 100) * -1)
+#         _, predictions = outputs.max(1)
+#         num_correct += (predictions ==  labels).sum()
+#         num_samples += predictions.size(0)
+#         running_loss += loss.item()
+#     train_acc = float(num_correct) / float(num_samples) * 100
+#     train_err.append((train_acc - 100) * -1)
 
-    # begin training
-    demdetect.eval()
-    for (points, labels) in testing_loader:
-        points = points.to(device)
-        points = points.unsqueeze_(2)
-        labels = labels.to(device)
-        outputs = demdetect(points)
+#     # begin training
+#     demdetect.eval()
+#     for (points, labels) in testing_loader:
+#         points = points.to(device)
+#         points = points.unsqueeze_(2)
+#         labels = labels.to(device)
+#         outputs = demdetect(points)
 
-        labels = torch.max(labels, 1)[1]
+#         labels = torch.max(labels, 1)[1]
 
-        if outputs.size() == 3:
-            outputs = outputs.reshape(3,1,3,1)
-        outputs = outputs.flatten(start_dim=1)
+#         if outputs.size() == 3:
+#             outputs = outputs.reshape(3,1,3,1)
+#         outputs = outputs.flatten(start_dim=1)
 
-        _, predictions = outputs.max(1)
-        t_num_correct += (predictions == labels).sum()
-        t_num_samples += predictions.size(0)
-    test_acc = float(t_num_correct) / float(t_num_samples) * 100
-    test_err.append((test_acc - 100) * -1)
-    print(f'[Epoch {epochs + 1}/{no_epochs}] Epoch Loss: {running_loss / len(training_loader):.3f} | Got {num_correct} of {num_samples} with accuracy {train_acc:.2f}%')
-    print(f'[Epoch {epochs + 1}/{no_epochs}] | Test: Got {t_num_correct} of {t_num_samples} with accuracy {test_acc:.2f}%')
-print('Finished Training')
+#         _, predictions = outputs.max(1)
+#         t_num_correct += (predictions == labels).sum()
+#         t_num_samples += predictions.size(0)
+#     test_acc = float(t_num_correct) / float(t_num_samples) * 100
+#     test_err.append((test_acc - 100) * -1)
+#     print(f'[Epoch {epochs + 1}/{no_epochs}] Epoch Loss: {running_loss / len(training_loader):.3f} | Got {num_correct} of {num_samples} with accuracy {train_acc:.2f}%')
+#     print(f'[Epoch {epochs + 1}/{no_epochs}] | Test: Got {t_num_correct} of {t_num_samples} with accuracy {test_acc:.2f}%')
+# print('Finished Training')
